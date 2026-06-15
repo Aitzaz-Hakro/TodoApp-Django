cd0744daeae30cdb516aab260af7961bef4635c6
Aitzaz Hassan <aitzazhakro123@gmail.com>
2026-06-15 14:02:41 +0500

    restricted api requests to GET only

--- myproject/TodoApp/api/views.py
+++ myproject/TodoApp/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from TodoApp.models import Task
 from TodoApp.api.serializers import TaskSerializer
 
+
+http_methods = ['GET']
 class TaskListCreate(generics.ListCreateAPIView):
     queryset = Task.objects.all()  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
     serializer_class = TaskSerializer
